339e944c25ae4d23955b733d884014c5edbef069
Lorenzo <templarec@gmail.com>
2021-05-26 16:41:17 +0200

    auth ok

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/', 'HomeController@index')->name('index');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+    });
